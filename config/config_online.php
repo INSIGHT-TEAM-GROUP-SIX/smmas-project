f153546f70337efe6522102830b803f9686d9127
Leonard-simon <ogenleo95@gmail.com>
2026-05-20 19:00:47 +0300

    Fixed database password

--- config/config_online.php
+++ config/config_online.php
@@ -8,6 +8,6 @@ if($is_online){
 define('DB_HOST', 'sql201.infinityfree.com');
 define('DB_NAME', 'if0_41961194_smmas_db1');
 define('DB_USER', 'if0_41961194');
-define('DB_PASS', 'G990KtgAJ1R4D0');
+define('DB_PASS', 'G99OKtgAJ1R4DO');
 }
 ?>
