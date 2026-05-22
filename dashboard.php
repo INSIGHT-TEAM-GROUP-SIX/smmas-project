0d8832b0e838f1f69d36d92b8052868bfd27902f
Ibrah <kiwafuibrahimwamala@gmail.com>
2026-05-22 14:38:01 +0300

    Fixed dashboard animation timing

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,28 @@ $recent = $conn->query("SELECT t.*, m.medicine_name, p.full_name FROM `transacti
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>Dashboard</title><link rel="stylesheet" href="css/style.css" /></head>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>Dashboard</title>
+<style>
+    .typing {
+      width: 22ch; /* number of characters */
+      animation: typing 3s steps(22), blink .5s step-end infinite alternate;
+      white-space: nowrap;
+      overflow: hidden;
+      border-right: 3px solid;
+      font-family: monospace;
+      font-size: 20px;
+    }
+
+    @keyframes typing {
+      from { width: 0 }
+      to { width: 22ch }
+    }
+
+    @keyframes blink {
+      50% { border-color: transparent }
+    }
+  </style>
+<link rel="stylesheet" href="css/style.css" /></head>
 <body>
 <?php include 'includes/navbar.php'; ?>
 <div class="container">
